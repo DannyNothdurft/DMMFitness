7b1f2ae97d05b38a9200c809047ff6aa72a78896
Danny Nothdurft <d.nothdurft@gmail.de>
2022-06-14 13:36:46 +0200

    React-Router-Dom installiert om installiert und den Shop verbunden

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from "../images/logo.png"
 // REACT FONTAWESOME IMPORTS 
 
@@ -6,42 +7,42 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
-    return (
-    
-            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-<div className="container d-flex flex-row-reverse">
+  return (
 
-  <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo"/> <span-logo><span>DMM</span>Fitness</span-logo></a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <FontAwesomeIcon icon={ faBars} style={{color: "white"}}/>
-  </button>
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="container d-flex flex-row-reverse">
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home |</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Who Are We? |</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Classes |</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#"> Pricing |</a>
-      </li>
-      <li className="nav-item active">
-        <a className="nav-link" href="#"> Shop |</a>
-        </li>
-        <li className="nav-item active">
-        <a className="nav-link" href="#">Find Studio </a>
-        </li>
-      
-    </ul>
-</div>
- 
-  </div>
-</nav>
-       
-    )
+        <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo" /> <span-logo><span>DMM</span>Fitness</span-logo></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <FontAwesomeIcon icon={faBars} style={{ color: "white" }} />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home |</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Who Are We? |</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Classes |</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Pricing |</a>
+            </li>
+            <li className="nav-item active">
+              <Link to='/shop'>Shop |</Link>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Find Studio</a>
+            </li>
+
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+
+  )
 }
---
+++ client/src/pages/Shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Shop() {
+    return (
+        < div id="myShop" >
+            <a href="https://dmm-fitness.myspreadshop.de">dmm-fitness</a>
+        </div >
+    )
+}
+
+export default Shop
