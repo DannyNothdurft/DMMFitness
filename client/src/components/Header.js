36dffc3799d0b44e1e72f8d860fbaad1f27329e0
Danny Nothdurft <d.nothdurft@gmail.de>
2022-06-14 14:13:50 +0200

    register Button eine Funktion gegeben, Register Componente Angelegt

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typed from "react-typed"
 
+// Components
+import Register from './Register'
+
 export default function Header() {
+    const [register, setRegister] = useState(false)
+
+    const openRegisterCard = () => {
+        setRegister(!register);
+    }
+
     return (
         <div className="header-wraper">
-            <video src="/videos/home3.mp4" autoPlay loop muted/>
-                <h1>Welcome in our Fitness World</h1>
-                <Typed 
+            <video src="/videos/home3.mp4" autoPlay loop muted />
+            <h1>Welcome in our Fitness World</h1>
+            <Typed
                 className="typed-text"
                 strings={["Dedicate.", "Motivate.", "Make Your First Step!."]}
                 typedSpeed={20}
                 backSpeed={80}
                 loop
-                />
-                <a href="" className="btn-main-offer"> Register</a>
-         
+            />
+            <button
+                className="btn-main-offer"
+                onClick={openRegisterCard}
+            >Register</button>
+
+            {register ? <Register /> : undefined}
         </div>
     )
 }
