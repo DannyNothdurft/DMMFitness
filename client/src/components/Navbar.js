26cc94c89e13dd8af30613c36d10363d1d11ae9a
Danny Nothdurft <d.nothdurft@gmail.de>
2022-06-14 13:51:13 +0200

    a Elemente durch Link elemente ausgetauscht

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -20,22 +20,22 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home |</a>
+              <Link to='/'>Home |</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Who Are We? |</a>
+              <Link to='/aboutus'>Who Are We? |</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Classes |</a>
+              <Link to='/classes'>Classes |</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Pricing |</a>
+              <Link to='/pricing'>Pricing |</Link>
             </li>
             <li className="nav-item active">
               <Link to='/shop'>Shop |</Link>
             </li>
             <li className="nav-item active">
-              <a className="nav-link" href="#">Find Studio</a>
+              <Link to='/findstudio'>Find Studio</Link>
             </li>
 
           </ul>
