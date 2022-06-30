1f4d7b510320c2e04b4d504ed28146ccb554dbc6
Magdelena <magdelenagavrilova@gmail.com>
2022-06-30 09:03:52 +0200

    ha

--- client/src/components/GlassCardMarija.js
+++ client/src/components/GlassCardMarija.js
@@ -1,37 +1,36 @@
-import React from 'react';
-import profile from '../images/marija.png';
-import styled from 'styled-components';
-import { useSpring, animated, config } from 'react-spring';
+import React from "react";
+import profile from "../images/marija.png";
+import styled from "styled-components";
+import { useSpring, animated, config } from "react-spring";
 
 const Container = styled(animated.div)`
-display: inline-block;
-padding: 3em;
-background: transparent;
-background-image: linear-gradient(15deg, transparent 0%, #d2ccc4 99%);
+  display: inline-block;
+  padding: 3em;
+  background: transparent;
+  background-image: linear-gradient(15deg, transparent 0%, #d2ccc4 99%);
 
-border-radius: 10px;
-z-index: 1;
-position: relative;
-backdrop-filter: blur(10px);
-border: 2px solid transparent;
-background-clip: border-box;
-cursor: pointer;
-width: 25%;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items:center;
-font-size: 0.5rem;
-margin-left: 2rem;
-margin-top: 3rem;
-
+  border-radius: 10px;
+  z-index: 1;
+  position: relative;
+  backdrop-filter: blur(10px);
+  border: 2px solid transparent;
+  background-clip: border-box;
+  cursor: pointer;
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 0.5rem;
+  margin-left: 2rem;
+  margin-top: 3rem;
 `;
 
 const StyledImg = styled.img`
-    width: 200px;
-    height: auto;
-     margin-top: -2rem;
-   /*  border: 2px solid #000;
+  width: 200px;
+  height: auto;
+  margin-top: -2rem;
+  /*  border: 2px solid #000;
     border-radius: 50%; */
 `;
 
@@ -42,31 +41,39 @@ const StyledImg = styled.img`
 `; */
 
 const StyledH3 = styled.h3`
-    line-heright: 1.5;
-    letter-spacing: 1.15;
-    font-family: "Gilroy";
-    font-size: 20px;
-    text-shadow: 2px 1px 6px white;
+  line-heright: 1.5;
+  letter-spacing: 1.15;
+  font-family: "Gilroy";
+  font-size: 20px;
+  text-shadow: 2px 1px 6px white;
 `;
 
-const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1]
-const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 20,
+  1,
+];
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
 const GlassCard = () => {
-    const [props, set] = useSpring(() => ({ xys: [0, 0, 1] , config: config.default}))
-    return (
-        <Container
-            onMouseMove={({clientX: x, clientY: y}) => (set({xys: calc(x, y)}))}
-            onMouseLeave={() => set({xys:[0,0,1]})}
-            style={{
-                transform: props.xys.interpolate(trans)
-            }}
-        >
-            <StyledImg src={profile} />
-            <h2 className="our-names">Marija Gavrilova</h2>
-            <StyledH3>Web Developer (Student by DCI)</StyledH3>
-        </Container>
-    );
-}
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: config.default,
+  }));
+  return (
+    <Container
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      style={{
+        transform: props.xys.interpolate(trans),
+      }}
+    >
+      <StyledImg src={profile} />
+      <h2 className="our-names">Marija Gavrilova</h2>
+      <StyledH3>Web Developer (Student by DCI)</StyledH3>
+    </Container>
+  );
+};
 
 export default GlassCard;
