742ba08988afa8a434ddbed3b16ba688db6dbecb
Marija <macenceee@yahoo.com>
2022-07-01 09:13:37 +0200

    studio search veränderung

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,44 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
 
 // Components
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-
-
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 // Pages
-import Shop from './pages/Shop';
-import MarkersMap from './pages/map/Map';
-import About from './pages/aboutUs/About'
+import Shop from "./pages/Shop";
+import MarkersMap from "./pages/map/Map";
+import About from "./pages/aboutUs/About";
 
 import Home from "./pages/classes/home/Home";
 import Hotel from "./pages/classes/hotel/Hotel";
-import List from "./pages/classes/list/List";
+import List from "./pages/classes/list/List"; 
 import Login from "./pages/classes/login/Login";
-import Register from './components/register/Register'
+import Register from "./components/register/Register";
 function App() {
- 
   return (
     <>
-    <BrowserRouter>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
 
-      <Routes>
-      
-		
-		
-			
-        <Route path='/' element={<Header />} />
-        <Route path='/shop' element={<Shop />} />
-      
-        <Route path='/locations' element={
-      <MarkersMap />
-   } />
- 
-        <Route path="/classes" element={<Home/>}/>
-        <Route path="/studios" element={<List/>}/>
-        <Route path="/studios/:id" element={<Hotel/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path='/aboutus' element={<About />} />
-        <Route path='/' exact element={<Register />}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/shop" element={<Shop />} />
 
-    </BrowserRouter>
-   
-         
-    
-   </>
+          <Route path="/locations" element={<MarkersMap />} />
+
+          <Route path="/classes" element={<Home />} />
+           <Route path="/studios" element={<List />} />
+          <Route path="/studios/:id" element={<Hotel />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/aboutus" element={<About />} />
+          <Route path="/" exact element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
-
--- client/src/pages/classes/list/List.js
+++ client/src/pages/classes/list/List.js
@@ -1,4 +1,4 @@
-import "./list.css";
+ import "./list.css";
 import MailList from "../../../components/mailList/MailList.js";
 import Footer from "../../../components/footer/Footer.js"; 
 import { useLocation } from "react-router-dom";
@@ -26,89 +26,10 @@ const List = () => {
   };
 
   return (
-    <div>
-      {/* <Navbar />
-      <Header type="list" /> */}
-      <div style={{backgroundColor: "#485461", backgroundImage: 'linear-gradient(315deg, #2f4353 0%, #d2ccc4 74%)'}} className="listContainer">
-        <div data-aos="fade-up" className="listWrapper">
-          <div style={{backgroundColor: "#485461"}} className="listSearch">
-            <h1 style={{color:'orange', marginTop:'-10px', fontSize:'2rem'}} className="lsTitle">Your Search</h1>
-            <div className="lsItem">
-              <label style={{color:'orange', fontSize:'1rem'}}>Studio Location</label>
-              <input placeholder={destination} type="text" />
-            </div>
-            <div className="lsItem">
-              <label style={{color:'orange', fontSize:'1rem'}}>Booked Classes Date</label>
-              <span style={{ borderRadius: '.3rem'}} onClick={() => setOpenDate(!openDate)}>{`${format(
-                dates[0].startDate,
-                "MM/dd/yyyy"
-              )} to ${format(dates[0].endDate, "MM/dd/yyyy")}`}</span>
-              {openDate && (
-                <DateRange
-                  onChange={(item) => setDates([item.selection])}
-                  minDate={new Date()}
-                  ranges={dates}
-                />
-              )}
-            </div>
-         {/*    <div className="lsItem">
-              <label>Options</label>
-              <div className="lsOptions">
-                <div className="lsOptionItem">
-                  <span className="lsOptionText">
-                    Min price <small>per night</small>
-                  </span>
-                  <input
-                    type="number"
-                    onChange={(e) => setMin(e.target.value)}
-                    className="lsOptionInput"
-                  />
-                </div>
-                <div className="lsOptionItem">
-                  <span className="lsOptionText">
-                    Max price <small>per night</small>
-                  </span>
-                  <input
-                    type="number"
-                    onChange={(e) => setMax(e.target.value)}
-                    className="lsOptionInput"
-                  />
-                </div>
-                <div className="lsOptionItem">
-                  <span className="lsOptionText">Adult</span>
-                  <input
-                    type="number"
-                    min={1}
-                    className="lsOptionInput"
-                    placeholder={options.adult}
-                  />
-                </div>
-                <div className="lsOptionItem">
-                  <span className="lsOptionText">Children</span>
-                  <input
-                    type="number"
-                    min={0}
-                    className="lsOptionInput"
-                    placeholder={options.children}
-                  />
-                </div>
-                <div className="lsOptionItem">
-                  <span className="lsOptionText">Room</span>
-                  <input
-                    type="number"
-                    min={1}
-                    className="lsOptionInput"
-                    placeholder={options.room}
-                  />
-                </div>
-              </div>
-              </div>*/}
-  
-            <button style={{backgroundColor: 'orange', color: 'black', fontSize:'20px', fontWeight:'bold'}} onClick={handleClick}>Search</button>
-          
-              </div> 
+    <div style={{backgroundColor: "#485461", backgroundImage: 'linear-gradient(315deg, #2f4353 0%, #d2ccc4 74%)'}} className="listContainer">
+     
           <div className="listResult">
-            
+           <h2> </h2>
           {loading ? (
               "loading"
             ) : (
@@ -119,17 +40,18 @@ const List = () => {
               </>
             )}
             
-            
+             </div>
              
           
-          </div>
-        </div>
+          
+       
         <MailList/>
       <Footer/>
-      </div>
+    
     
     </div> 
   );
 };
 
 export default List;
+ 
